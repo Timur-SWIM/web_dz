f191c647ebabcafad6d492a9407d98525bfa3e22
timur <tmyaldzin13@gmail.com>
2023-11-13 03:04:44 +0300

    add db models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,60 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    
+    def __str__(self):
+        return self.user.username
+
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    objects = models.Manager()  
+
+    class BestQuestionsManager(models.Manager):
+        def get_queryset(self):
+            
+            return super().get_queryset().filter(is_best=True)
+
+    class NewQuestionsManager(models.Manager):
+        def get_queryset(self):
+            
+            return super().get_queryset().order_by('-created_at')
+
+    best_objects = BestQuestionsManager()  
+    new_objects = NewQuestionsManager()    
+
+class Answer(models.Model):
+    content = models.TextField()
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Answer to '{self.question.title}'"
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer = models.ForeignKey(Answer, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.user.username} likes {self.question.title}{' - ' + self.answer.content if self.answer else ''}"
+
