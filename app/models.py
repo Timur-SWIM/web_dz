db8227880dc0c8d24b995117413d01e50e4c92de
Timur <tmyaldzin13@gmail.com>
2023-11-15 20:25:29 +0300

    edit views and fix bd models

--- app/models.py
+++ app/models.py
@@ -1,18 +1,42 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class TagManager(models.Manager):
+    def most_popular(self, total):
+        return self.all()[:total]
+
 class Tag(models.Model):
     name = models.CharField(max_length=50)
 
+    objects = TagManager()
+    
     def __str__(self):
         return self.name
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     bio = models.TextField(blank=True)
     
     def __str__(self):
         return self.user.username
+
+class QuestionsManager(models.Manager):
+        def newest(self):
+            return [{'question': q, 'tags': self.get_tags(q.pk)} for q in self.order_by('-creation_date')]
+    
+        def best(self):
+            return [{'question': q, 'tags': self.get_tags(q.pk)} for q in self.order_by('-rating')]
+    
+        def with_tag(self, tag_name):
+            return [{'question': q, 'tags': self.get_tags(q.pk)} for q in self.filter(tags=tag_name)]
+    
+        def with_id(self, id):
+            q = self.get(pk=id)
+            return {'question': q, 'tags': self.get_tags(q.pk)}
+    
+        def get_tags(self, id):
+            tag_names = self.values_list('tags', flat=True).filter(pk=id)
+            return Tag.objects.filter(pk__in=tag_names)  
 
 class Question(models.Model):
     title = models.CharField(max_length=255)
@@ -22,23 +46,17 @@ class Question(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    objects = QuestionsManager()
+
     def __str__(self):
         return self.title
 
-    objects = models.Manager()  
+class AnswerManager(models.Manager):
+        def newest(self, question_id):
+            return self.filter(question=question_id).order_by('-creation_date')
 
-    class BestQuestionsManager(models.Manager):
-        def get_queryset(self):
-            
-            return super().get_queryset().filter(is_best=True)
-
-    class NewQuestionsManager(models.Manager):
-        def get_queryset(self):
-            
-            return super().get_queryset().order_by('-created_at')
-
-    best_objects = BestQuestionsManager()  
-    new_objects = NewQuestionsManager()    
+        def best(self, question_id):
+            return self.filter(question=question_id).order_by( '-is_correct', '-rating')
 
 class Answer(models.Model):
     content = models.TextField()
@@ -46,6 +64,8 @@ class Answer(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    objects = AnswerManager()
 
     def __str__(self):
         return f"Answer to '{self.question.title}'"
--- app/views.py
+++ app/views.py
@@ -1,43 +1,9 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator
+from app.models import Question, Answer, Tag
 from random import randint
 
 # Create your views here.
-TAGS = {
-    'Python': {'name': 'Python', 'bg': 'bg-primary'},
-    'Cpp': {'name': 'Cpp', 'bg': 'bg-primary'},
-    'Google': {'name': 'Google', 'bg': 'bg-danger'},
-    'Bootstrap': {'name': 'Bootstrap', 'bg': 'bg-primary'},
-    'Dinner': {'name': 'Dinner', 'bg': 'bg-warning'},
-    'Animals': {'name': 'Animals', 'bg': 'bg-warning'},
-    'Law': {'name': 'Law', 'bg': 'bg-warning'},
-    'git': {'name': 'git', 'bg': 'bg-primary'},
-    'Languages': {'name': 'Languages', 'bg': 'bg-danger'},
-}
-
-ANSWERS = [
-    {
-        'id': i,
-        'content': '''Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident \
-            alias libero reprehenderit possimus, dolore modi consequuntur placeat enim error \
-            suscipit vitae officiis iure in totam dignissimos nulla eius, quaerat aliquid?''',
-        'rating': randint(-40, 40),
-        'is_correct': i == 0
-    } for i in range(40)
-]
-
-QUESTIONS = [
-    {
-        'id': i,
-        'user': f'Bobo_{i}',
-        'tags': [tag for tag in list(TAGS.values())[::3]],
-        'rating': randint(-40, 40),
-        'title': f'Question ({i})???',
-        'content': f'''Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident alias libero reprehenderit possimus, \
-dolore modi consequuntur placeat enim error suscipit vitae officiis iure in totam dignissimos nulla eius, quaerat aliquid ({i})?'''
-    } for i in range(100)
-]
-
 
 def paginate(objects, request, per_page=5):
     page = str(request.GET.get('page', 1))
@@ -49,22 +15,32 @@ def paginate(objects, request, per_page=5):
 
 # New
 def index(request):
-    return render(request, 'index.html', {'page': paginate(QUESTIONS, request), 'tags': TAGS.values(), 'is_logged_in': True, 'component_to_paginate': 'components/question.html'})
+    questions = Question.objects.newest()
+    tags = Tag.objects.most_popular(10)
+    return render(request, 'index.html', {'page': paginate(questions, request), 'tags': tags, 'is_logged_in': True, 'component_to_paginate': 'components/question.html'})
 
 # Hot
 def hot_questions(request):
-    return render(request, 'index_hot.html', {'tags': TAGS.values(), 'page': paginate(QUESTIONS, request), 'component_to_paginate': 'components/question.html'})
+    questions = Question.objects.hot_questions()
+    tags = Tag.objects.most_popular(10)
+    return render(request, 'index_hot.html', {'tags': tags, 'page': paginate(questions, request), 'component_to_paginate': 'components/question.html'})
 
 # Tag
 def tag(request, tag_name):
-    tag_item = TAGS[tag_name] if tag_name in TAGS else TAGS['Animals']
-    questions = [QUESTIONS[i] for i in range(len(QUESTIONS)) if i % 3 == 0]
-    return render(request, 'index_tags.html', {'tag_item': tag_item, 'tags': TAGS.values(), 'page': paginate(questions, request), 'component_to_paginate': 'components/question.html'})
+    try:
+        tag_item = Tag.objects.get(pk=name)
+    except Tag.DoesNotExist:
+        tag_item = Tag.objects.all()[0]
+    questions = Question.objects.with_tag(tag_item.name)
+    tags = Tag.objects.most_popular(10)
+    return render(request, 'index_tags.html', {'tag_item': tag_item, 'tags': tags, 'page': paginate(questions, request), 'component_to_paginate': 'components/question.html'})
 
 # Question
 def question(request, question_id):
-    question_item = QUESTIONS[question_id] if 0 <= question_id and question_id < len(QUESTIONS) else QUESTIONS[0]
-    return render(request, 'question.html', {'question': question_item, 'tags': TAGS.values(), 'page': paginate(ANSWERS, request), 'component_to_paginate': 'components/answer.html'})
+    question_item = Question.objects.with_id(question_id)
+    answers = Answer.objects.best(question_id)
+    tags = Tag.objects.most_popular(10)
+    return render(request, 'question.html', {'question': question_item, 'tags': tags, 'page': paginate(answers, request), 'component_to_paginate': 'components/answer.html'})
 
 # Log In
 def login(request):
